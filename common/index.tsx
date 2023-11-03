d619a0eebcccd080ae8407fdf2503c0bd4e74a27
Yash Sehgal <yashsehgal.work@gmail.com>
2023-11-03 23:56:02 +0530

    🔧 Add config for env and common

--- common/index.tsx
+++ common/index.tsx
@@ -1,3 +1,5 @@
 
 export const BASEFOLIO_LOGO = "/basefolio-logo.svg";
 export const BASEROUTE = process.env.NEXT_PUBLIC_BASE_ROUTE ?? "/";
+export const STRAPI_HOSTNAME = process.env.NEXT_PUBLIC_STRAPI_BASE_URL;
+export const STRAPI_BASE_API_URL = STRAPI_HOSTNAME + "/api";
